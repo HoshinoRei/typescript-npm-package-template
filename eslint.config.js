a3fc5e97a58a60df1c2c2483d74e11912504d894
HoshinoRei <81515470+HoshinoRei@users.noreply.github.com>
2024-10-31 09:52:42 +0800

    chore(eslint): replace ESLint configuration with new setup (#155)
    - remove `.eslintrc.yml` in favor of a new `eslint.config.js` file for a modern configuration
    - update `package.json` to use `@eslint/js` and adjust ESLint plugins
    - simplify linting commands in `package.json` for better usability
    - add `typescript-eslint` for TypeScript support within the new ESLint setup

---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+import pluginJs from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import importPlugin from "eslint-plugin-import";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import globals from "globals";
+// eslint-disable-next-line import/no-unresolved
+import tseslint from "typescript-eslint";
+
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintConfigPrettier,
+  importPlugin.flatConfigs.recommended,
+  {
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+    },
+  },
+];
